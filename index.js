5dd6fda4953bdc1739475b48daf4444086803c74
jrajav <jrajav@gmail.com>
2014-06-26 16:03:02 -0500

    A lot

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 
 var fs = require( 'fs' )
 var path = require( 'path' )
@@ -6,9 +7,14 @@ var _ = require( 'lodash' )
 var rimraf = require( 'rimraf' )
 var sourcetrace = require( 'sourcetrace' )
 
+
 var jshintDir = process.argv[2]
 
+var messages = require( path.join( jshintDir, 'src', 'messages' ) )
 
+
+
+// For async funcs, throw any error or continue with callback
 function e( callback ) {
 
 	return function ( err ) {
@@ -24,15 +30,11 @@ function e( callback ) {
 }
 
 
-function filter( file ) {
-	return path.extname( file ) === '.js' && path.basename( file ) !== 'messages.js'
-}
-
 
 function formatTrace( trace, filename, fileLines ) {
 
 	var githubUrl = 'https://github.com/jshint/jshint/blob/master/src/'
-	var lastLine = 0
+	var lastline
 
 	function padLineNumber( line ) {
 		line = String( line )
@@ -41,99 +43,157 @@ function formatTrace( trace, filename, fileLines ) {
 
 	return '<pre>' + _.reduce( trace, function ( result, line ) {
 
-		if ( lastLine !== line - 1 ) {
+		if ( lastline && lastline !== line - 1 ) {
 			result += '…\n'
 		}
+
+		lastline = line
 
 		result += '<a href="' + githubUrl + filename + '#L' + line + '">'
 
 		result += padLineNumber( line ) + ': '
 
-		result += fileLines[ line - 1 ]
+		result += fileLines[ line - 1 ].replace( /\s+$/g, '' )
 
 		result += '</a>\n'
 
-	}, '' ) + '</pre>'
+		return result
+
+	}, '' ) + '</pre>\n'
 
 }
 
 
-var messages = require( path.join( jshintDir, 'src', 'messages' ) )
+function formatMessage( files, message ) {
+
+	var traces = _.mapValues( files, function ( file, filename ) {
+
+		var fileTraces
+
+		try {
+			fileTraces = sourcetrace( '"' + message.code + '"', file )
+		} catch (e) {
+			e.message = 'Error tracing ' + filename + ':\n' + e.message
+			throw e
+		}
+
+		return _.uniq( _.flatten( fileTraces ) )
+
+	} )
+
+	var fileLines = _.mapValues( files, function (file) { return file.split( '\n' ) } )
+
+	var options = _.reduce( traces, function ( result, trace, filename ) {
+
+		var lines = fileLines[filename]
+
+		_.each( trace, function ( line ) {
+
+			var fileLine = lines[ line - 1 ]
+			var optionRegex = /state\.option\.([a-zA-Z_$][0-9a-zA-Z_$]*)/g
+			var regexResult
+
+			if ( _.contains( fileLine, 'state.option' ) ) {
+
+				while ( ( regexResult = optionRegex.exec( fileLine ) ) !== null ) {
+					result.push( regexResult[1] )
+				}
+
+			}
+
+		} )
+
+		return result
+
+	}, [] )
+
+
+	var result = ''
+
+	result += '# ' + message.code + '\n\n'
+
+	result += '###### ' + ( message.desc || '*Retired message - No description*' ) + '\n\n'
+
+	if ( options.length ) {
+
+		result += '## Affecting options\n\n'
+
+		result += _.map( options, function ( option ) {
+			return '<a href="http://www.jshint.com/docs/options/#' + option + '">' + option + '</a>'
+		} ).join( '\n' ) + '\n\n'
+	}
+
+	if ( _.any( traces, function ( trace ) { return !_.isEmpty( trace ) } ) ) {
+
+		result += '## Source\n\n'
+
+		result += _( traces ).pick( function ( trace ) {
+			return !_.isEmpty( trace )
+		} ).map( function ( trace, filename ) {
+			return '### ' + filename + '\n' + formatTrace( trace, filename, fileLines[filename] ) + '\n\n'
+		} ).join( '\n' )
+
+	}
+
+	return result
+
+}
+
 
 
 function processMessage( dir, files, message ) {
 
-	var code = message.code
-	var desc = message.desc
+	process.stdout.write( message.code + ' ' )
 
-	fs.writeFile( path.join( dir, code + '.md' ),
-		'# ' + code + '\n' + desc + '\n' +
-		_.reduce( files, function ( result, file, filename ) {
-
-			console.log( code + ' - ' + filename )
-
-			var trace, lines
-
-			try {
-				var traces = sourcetrace( '"' + code + '"', file )
-			} catch (e) {
-				e.message = 'Error reading ' + filename + ':\n' + e.message
-				throw e
-			}
-
-			if ( traces.length ) {
-
-				lines = file.split( '\n' )
-
-				result += _.map( traces, function ( trace ) {
-					return '\n' + '## ' + filename + '\n' + formatTrace( trace, filename, lines )
-				} ).join( '\n' )
-
-			}
-
-			return result
-
-		}, '' )
-	)
+	fs.writeFile( path.join( dir, message.code + '.md' ), formatMessage( files, message ) )
 
 }
 
 
 function done( files ) {
 
-	rimraf( 'errors', e( function () {
-		fs.mkdir( 'errors', e( function () {
-			_.each( messages.errors, _.curry( processMessage )( 'errors' )( files ) )
+	function process( message ) {
+		rimraf( message, e( function () {
+			fs.mkdir( message, e( function () {
+				_.each( messages[message], _.curry( processMessage )( message )( files ) )
+			} ) )
 		} ) )
-	} ) )
+	}
 
-	rimraf( 'warnings', e( function () {
-		fs.mkdir( 'warnings', e( function () {
-			_.each( messages.warnings, _.curry( processMessage )( 'warnings' )( files) )
-		} ) )
-	} ) )
+	process( 'errors' )
+	process( 'warnings' )
+	process( 'info' )
 
 }
 
 
-fs.readdir( path.join( jshintDir, 'src' ), e( function ( files ) {
 
-	files = _.filter( files, filter )
+fs.readdir( path.join( jshintDir, 'src' ), e( function ( filepaths ) {
 
-	var filestrings = {}
+	filepaths = _.filter( filepaths, function ( file ) {
+		return path.extname( file ) === '.js' && path.basename( file ) !== 'messages.js'
+	} )
 
-	var doneFn = _.after( files.length, done )
+	var files = {}
 
-	_.each( files, function ( file ) {
+	var doneFn = _.after( filepaths.length, done )
+
+	_.each( filepaths, function ( file ) {
 
 		fs.readFile( path.join( jshintDir, 'src', file ), 'utf8', e( function ( filestring ) {
 
-			filestrings[ file ] = filestring
+			files[ file ] = filestring
 
-			doneFn( filestrings )
+			doneFn( files )
 
 		} ) )
 
 	} )
 
 } ) )
+
+
+
+process.on( 'exit', function () {
+	process.stdout.write( '\n' )
+})
